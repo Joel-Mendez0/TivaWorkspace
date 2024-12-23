d66f56764817519bd2bcc40b8e578680816aef62
joel <jmendez04@manhattan.edu>
2024-12-23 14:41:55 -0500

    Test

---
+++ Test/Debug/main.d
@@ -0,0 +1,48 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+main.obj: /home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+main.obj: /home/joel/ti/Tiva_C_Series/inc/hw_memmap.h
+main.obj: /home/joel/ti/Tiva_C_Series/driverlib/sysctl.h
+main.obj: /home/joel/ti/Tiva_C_Series/driverlib/gpio.h
+
+../main.c:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+/home/joel/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+/home/joel/ti/Tiva_C_Series/inc/hw_memmap.h:
+
+/home/joel/ti/Tiva_C_Series/driverlib/sysctl.h:
+
+/home/joel/ti/Tiva_C_Series/driverlib/gpio.h:
+
---
+++ Test/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -llibc.a -l"/home/joel/ti/Tiva_C_Series/driverlib/ccs/Debug/driverlib.lib"
+
