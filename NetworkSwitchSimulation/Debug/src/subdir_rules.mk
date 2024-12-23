a7e2bef43e2cfff1b7d26759ff94c2aae0018d3f
Joel-Mendez0 <113056408+Joel-Mendez0@users.noreply.github.com>
2024-12-23 12:36:58 -0500

    Take

--- NetworkSwitchSimulation/Debug/src/subdir_rules.mk
+++ NetworkSwitchSimulation/Debug/src/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 src/%.obj: ../src/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/Joel/workspace_v12/NetworkSwitchSimulation" --include_path="C:/ti/TivaWare_C_Series-2.2.0.295/inc" --include_path="C:/ti/TivaWare_C_Series-2.2.0.295" --include_path="C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include" --include_path="C:/Users/Joel/workspace_v12/NetworkSwitchSimulation/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="src/$(basename $(<F)).d_raw" --obj_directory="src" $(GEN_OPTS__FLAG) "$<"
+	"" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/Joel/workspace_v12/NetworkSwitchSimulation" --include_path="C:/ti/TivaWare_C_Series-2.2.0.295/inc" --include_path="C:/ti/TivaWare_C_Series-2.2.0.295" --include_path="/include" --include_path="C:/Users/Joel/workspace_v12/NetworkSwitchSimulation/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="src/$(basename $(<F)).d_raw" --obj_directory="src" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
--- NetworkSwitchSimulation/src/main.c
+++ NetworkSwitchSimulation/src/main.c
@@ -10,23 +10,23 @@ int main(void) {
     ConfigureI2C();
     LCDInit();
 
-//    uint32_t SystemClock = SysCtlClockGet();
-//    char buffer[10];
-//    itoa(SystemClock, buffer);
-//    UARTPrint(UART0_BASE, "System Clock: ");
-//    UARTPrint(UART0_BASE, buffer);
-//    UARTPrint(UART0_BASE, " Hz\r\n");
-//
-//    uint8_t data[10] = {1,2,3,4,5,6,7,8,9,10};
-//    uint8_t length = sizeof(data) / sizeof(data[0]);
-//
-//    I2CSendData(I2C0_BASE, LCD_ADDRESS, data, length);
-//
-//    LCDclear();
-//    LCDSendString("Bruh Take");
-//    LCDCur(1, 0);
-//    LCDSendString("Take Take");
-//
+    uint32_t SystemClock = SysCtlClockGet();
+    char buffer[10];
+    itoa(SystemClock, buffer);
+    UARTPrint(UART0_BASE, "System Clock: ");
+    UARTPrint(UART0_BASE, buffer);
+    UARTPrint(UART0_BASE, " Hz\r\n");
+
+    uint8_t data[10] = {1,2,3,4,5,6,7,8,9,10};
+    uint8_t length = sizeof(data) / sizeof(data[0]);
+
+    I2CSendData(I2C0_BASE, LCD_ADDRESS, data, length);
+
+    LCDclear();
+    LCDSendString("Take Take");
+    LCDCur(1, 0);
+    LCDSendString("Take Take");
+
 
 
 }
